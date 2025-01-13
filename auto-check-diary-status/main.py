687968cc3d2a9ebab6dbd1ee754f77b2a4d1ce69
Jason Liao <liao.jason2@gmail.com>
2025-01-13 11:18:19 +0800

    Modify to Google Cloud function format

--- auto-check-diary-status/main.py
+++ auto-check-diary-status/main.py
@@ -2,6 +2,8 @@ import requests
 import datetime
 import os
 from dotenv import load_dotenv
+import functions_framework
+
 
 load_dotenv()
 
@@ -19,6 +21,20 @@ headers = {
 def get_today_date():
     return datetime.datetime.now().strftime("%Y-%m-%d")
 
+@functions_framework.http
+def diary_check(request):
+    NOTION_API_TOKEN = os.getenv("NOTION_API_TOKEN")
+    DATABASE_ID = os.getenv("DATABASE_ID")
+
+    headers = {
+        "Authorization": f"Bearer {NOTION_API_TOKEN}",
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28",
+    }
+
+    def get_today_date():
+        return datetime.datetime.now().strftime("%Y-%m-%d")
+
     today_date = get_today_date()
     query_url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
     response = requests.post(query_url, headers=headers)
@@ -26,4 +42,8 @@ def get_today_date():
     if response.status_code == 200:
         results = response.json()["results"]
         title = "${date} 日記"
+        if (results[0]["properties"]["標題"]["title"][0]["text"]["content"]) != title:
+            return "Your diary is filled for today."
+        return "No diary entry found for today or it's empty."
     else:
+        return f"Error: {response.status_code}, {response.text}"
