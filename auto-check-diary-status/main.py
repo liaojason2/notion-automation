0ed24a023e2bd2cc24a1d8b1e3c2899a14a0029e
Jason Liao <liao.jason2@gmail.com>
2025-01-13 11:14:41 +0800

    Switch .git directory

--- auto-check-diary-status/main.py
+++ auto-check-diary-status/main.py
@@ -19,25 +19,11 @@ headers = {
 def get_today_date():
     return datetime.datetime.now().strftime("%Y-%m-%d")
 
-# Function to check if diary entry exists for today and is filled
-def check_diary_entry():
     today_date = get_today_date()
-
-    # Query the Notion database to check for today's entry
     query_url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
     response = requests.post(query_url, headers=headers)
 
     if response.status_code == 200:
         results = response.json()["results"]
-        
-        # If title is not "${date} 日記", means it have been filled
         title = "${date} 日記"
-        if(results[0]["properties"]["標題"]["title"][0]["text"]["content"]) != title:
-          print("Your diary is filled for today.")
-          return
-        print("No diary entry found for today or it's empty.")
     else:
-        print(f"Error: {response.status_code}, {response.text}")
-
-# Run the check
-check_diary_entry()
