94f9d9b37d8cdfc73cfb497f6739053355c13514
Jason Liao <liao.jason2@gmail.com>
2025-01-15 16:48:26 +0800

    sent notify by telegram bot

--- auto-check-diary-status/main.py
+++ auto-check-diary-status/main.py
@@ -1,9 +1,10 @@
 import requests
 import datetime
 import os
+import asyncio
 from dotenv import load_dotenv
 import functions_framework
-
+from telegrambot import send_message
 
 load_dotenv()
 
@@ -23,18 +24,6 @@ def get_today_date():
 
 @functions_framework.http
 def diary_check(request):
-    NOTION_API_TOKEN = os.getenv("NOTION_API_TOKEN")
-    DATABASE_ID = os.getenv("DATABASE_ID")
-
-    headers = {
-        "Authorization": f"Bearer {NOTION_API_TOKEN}",
-        "Content-Type": "application/json",
-        "Notion-Version": "2022-06-28",
-    }
-
-    def get_today_date():
-        return datetime.datetime.now().strftime("%Y-%m-%d")
-
     today_date = get_today_date()
     query_url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
     response = requests.post(query_url, headers=headers)
@@ -42,8 +31,11 @@ def diary_check(request):
     if response.status_code == 200:
         results = response.json()["results"]
         title = "${date} 日記"
-        if (results[0]["properties"]["標題"]["title"][0]["text"]["content"]) != title:
+
+        if results[0]["properties"]["標題"]["title"][0]["text"]["content"] != title:
+            asyncio.run(send_message(msg="Your diary is filled for today."))
             return "Your diary is filled for today."
+        asyncio.run(send_message(msg="No diary entry found for today or it's empty."))
         return "No diary entry found for today or it's empty."
     else:
         return f"Error: {response.status_code}, {response.text}"
--- auto-check-diary-status/telegrambot.py
+++ auto-check-diary-status/telegrambot.py
@@ -9,23 +9,13 @@ load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-async def send_message(msg: str, chat_id: str, token: str) -> None:
+async def send_message(msg: str) -> None:
     """
-    Sends a message to a Telegram user or group.
-
-    Args:
-        msg (str): Text of the message to be sent. Max 4096 characters.
-        chat_id (str): Unique identifier or username (e.g., '@channelusername').
-        token (str): Bot's unique authentication token.
+    Send a message to a Telegram chat
     """
     try:
-        bot = telegram.Bot(token=token)
-        await bot.send_message(chat_id=chat_id, text=msg)
+        bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+        await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=msg)
         print("Message Sent!")
     except TelegramError as e:
         print(f"Failed to send message: {e}")
-
-if __name__ == "__main__":
-    message_string = "Testing from virtual server"
-    print(message_string)
-    asyncio.run(send_message(msg=message_string, chat_id=TELEGRAM_CHAT_ID, token=TELEGRAM_BOT_TOKEN))
